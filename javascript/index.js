e8e447da29805a9962738ed02362d36a4efb7822
Alicia Urdiales <aliurdialess@gmail.com>
2023-10-30 17:03:54 +0000

    Iteration 3 done

--- javascript/index.js
+++ javascript/index.js
@@ -5,7 +5,6 @@
 
 // Out of sync
 /*getInstruction("mashedPotatoes", 0, (step1) => {
-/*getInstruction("mashedPotatoes", 0, (step1) => {
     document.querySelector("#mashedPotatoes").innerHTML += `<li>${step1}</li>`;
   }, (error) => console.log(error));
   
@@ -131,11 +130,35 @@ obtainInstruction("steak", 0)
   })
   .then((step8) => {
     document.querySelector("#steak").innerHTML += `<li>${step8}</li>`;
-    return obtainInstruction("steak", 9);
   });
 
 // Iteration 3 using async/await
-// ...
+
+async function makeBroccoli() {
+  try{
+    let step0 = await obtainInstruction("broccoli", 0)
+    document.querySelector("#broccoli").innerHTML += `<li>${step0}</li>`;
+    let step1 = await obtainInstruction("broccoli", 1)
+    document.querySelector("#broccoli").innerHTML += `<li>${step1}</li>`;
+    let step2 = await obtainInstruction("broccoli", 2)
+    document.querySelector("#broccoli").innerHTML += `<li>${step2}</li>`;
+    let step3 = await obtainInstruction("broccoli", 3)
+    document.querySelector("#broccoli").innerHTML += `<li>${step3}</li>`;
+    let step4 = await obtainInstruction("broccoli", 4)
+    document.querySelector("#broccoli").innerHTML += `<li>${step4}</li>`;
+    let step5 = await obtainInstruction("broccoli", 5)
+    document.querySelector("#broccoli").innerHTML += `<li>${step5}</li>`;
+    let step6 = await obtainInstruction("broccoli", 6)
+    document.querySelector("#broccoli").innerHTML += `<li>${step6}</li>`;
+    broccoli.push("Broccoli is ready!");
+    let step7 = await obtainInstruction("broccoli", 7)
+    document.querySelector("#broccoli").innerHTML += `<li>${step7}</li>`;
+
+  }catch(err){
+    console.log(err);
+  }
+}
+makeBroccoli();
 
 // Bonus 2 - Promise all
 // ...
